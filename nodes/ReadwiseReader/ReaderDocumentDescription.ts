fb1feeefafd1093db5f4838c2796cbe506cd7f3f
Bryan <git@singee.me>
2025-06-23 20:29:34 +0800

    fix lint

--- nodes/ReadwiseReader/ReaderDocumentDescription.ts
+++ nodes/ReadwiseReader/ReaderDocumentDescription.ts
@@ -86,48 +86,6 @@ export const documentFields: INodeProperties[] = [
         },
         options: [
             {
-                displayName: 'HTML',
-                name: 'html',
-                type: 'string',
-                default: '',
-                description: 'The document\'s content, in valid html. If you don\'t provide this, we will try to scrape the URL you provided to fetch html from the open web.',
-                routing: {
-                    request: {
-                        body: {
-                            html: '={{$value}}'
-                        }
-                    }
-                }
-            },
-            {
-                displayName: 'Should Clean HTML',
-                name: 'should_clean_html',
-                type: 'boolean',
-                default: false,
-                description: 'Whether to automatically clean the html and parse the metadata (title/author) of the document for you. Only valid when html is provided.',
-                routing: {
-                    request: {
-                        body: {
-                            should_clean_html: '={{$value}}'
-                        }
-                    }
-                }
-            },
-            {
-                displayName: 'Title',
-                name: 'title',
-                type: 'string',
-                default: '',
-                description: 'The document\'s title, it will overwrite the original title of the document',
-                routing: {
-                    request: {
-                        body: {
-                            title: '={{$value}}'
-                        }
-                    }
-                }
-            },
-            {
                 displayName: 'Author',
                 name: 'author',
                 type: 'string',
@@ -137,80 +95,6 @@ export const documentFields: INodeProperties[] = [
                     request: {
                         body: {
                             author: '={{$value}}'
-                        }
-                    }
-                }
-            },
-            {
-                displayName: 'Summary',
-                name: 'summary',
-                type: 'string',
-                default: '',
-                description: 'Summary of the document',
-                routing: {
-                    request: {
-                        body: {
-                            summary: '={{$value}}'
-                        }
-                    }
-                }
-            },
-            {
-                displayName: 'Published Date',
-                name: 'published_date',
-                type: 'dateTime',
-                default: '',
-                description: 'A datetime representing when the document was published in the ISO 8601 format; default timezone is UTC. Example: "2020-07-14T20:11:24+00:00".',
-                routing: {
-                    request: {
-                        body: {
-                            published_date: '={{$value}}'
-                        }
-                    }
-                }
-            },
-            {
-                displayName: 'Image URL',
-                name: 'image_url',
-                type: 'string',
-                default: '',
-                description: 'An image URL to use as cover image',
-                routing: {
-                    request: {
-                        body: {
-                            image_url: '={{$value}}'
-                        }
-                    }
-                }
-            },
-            {
-                displayName: 'Location',
-                name: 'location',
-                type: 'options',
-                default: 'new',
-                options: [
-                    {
-                        name: 'New',
-                        value: 'new',
-                    },
-                    {
-                        name: 'Later',
-                        value: 'later',
-                    },
-                    {
-                        name: 'Archive',
-                        value: 'archive',
-                    },
-                    {
-                        name: 'Feed',
-                        value: 'feed',
-                    },
-                ],
-                description: 'Represents the initial location of the document (previously called triage_status). Note: if you try to use a location the user doesn\'t have enabled in their settings, this value will be set to their default location.',
-                routing: {
-                    request: {
-                        body: {
-                            location: '={{$value}}'
                         }
                     }
                 }
@@ -268,6 +152,94 @@ export const documentFields: INodeProperties[] = [
                 }
             },
             {
+                displayName: 'HTML',
+                name: 'html',
+                type: 'string',
+                default: '',
+                description: 'The document\'s content, in valid html. If you don\'t provide this, we will try to scrape the URL you provided to fetch html from the open web.',
+                routing: {
+                    request: {
+                        body: {
+                            html: '={{$value}}'
+                        }
+                    }
+                }
+            },
+            {
+                displayName: 'Image URL',
+                name: 'image_url',
+                type: 'string',
+                default: '',
+                description: 'An image URL to use as cover image',
+                routing: {
+                    request: {
+                        body: {
+                            image_url: '={{$value}}'
+                        }
+                    }
+                }
+            },
+            {
+                displayName: 'Location',
+                name: 'location',
+                type: 'options',
+                default: 'new',
+                options: [
+                    {
+                        name: 'New',
+                        value: 'new',
+                    },
+                    {
+                        name: 'Later',
+                        value: 'later',
+                    },
+                    {
+                        name: 'Archive',
+                        value: 'archive',
+                    },
+                    {
+                        name: 'Feed',
+                        value: 'feed',
+                    },
+                ],
+                description: 'Represents the initial location of the document (previously called triage_status). Note: if you try to use a location the user doesn\'t have enabled in their settings, this value will be set to their default location.',
+                routing: {
+                    request: {
+                        body: {
+                            location: '={{$value}}'
+                        }
+                    }
+                }
+            },
+            {
+                displayName: 'Notes',
+                name: 'notes',
+                type: 'string',
+                default: '',
+                description: 'A top-level note of the document',
+                routing: {
+                    request: {
+                        body: {
+                            notes: '={{$value}}'
+                        }
+                    }
+                }
+            },
+            {
+                displayName: 'Published Date',
+                name: 'published_date',
+                type: 'dateTime',
+                default: '',
+                description: 'A datetime representing when the document was published in the ISO 8601 format; default timezone is UTC. Example: "2020-07-14T20:11:24+00:00".',
+                routing: {
+                    request: {
+                        body: {
+                            published_date: '={{$value}}'
+                        }
+                    }
+                }
+            },
+            {
                 displayName: 'Saved Using',
                 name: 'saved_using',
                 type: 'string',
@@ -277,6 +249,34 @@ export const documentFields: INodeProperties[] = [
                     request: {
                         body: {
                             saved_using: '={{$value}}'
+                        }
+                    }
+                }
+            },
+            {
+                displayName: 'Should Clean HTML',
+                name: 'should_clean_html',
+                type: 'boolean',
+                default: false,
+                description: 'Whether to automatically clean the html and parse the metadata (title/author) of the document for you. Only valid when html is provided.',
+                routing: {
+                    request: {
+                        body: {
+                            should_clean_html: '={{$value}}'
+                        }
+                    }
+                }
+            },
+            {
+                displayName: 'Summary',
+                name: 'summary',
+                type: 'string',
+                default: '',
+                description: 'Summary of the document',
+                routing: {
+                    request: {
+                        body: {
+                            summary: '={{$value}}'
                         }
                     }
                 }
@@ -296,15 +296,15 @@ export const documentFields: INodeProperties[] = [
                 }
             },
             {
-                displayName: 'Notes',
-                name: 'notes',
+                displayName: 'Title',
+                name: 'title',
                 type: 'string',
                 default: '',
-                description: 'A top-level note of the document',
+                description: 'The document\'s title, it will overwrite the original title of the document',
                 routing: {
                     request: {
                         body: {
-                            notes: '={{$value}}'
+                            title: '={{$value}}'
                         }
                     }
                 }
